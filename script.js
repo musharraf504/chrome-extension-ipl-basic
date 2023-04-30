d8bf8add15ad217a08b05ed7baa53844e7b8a03c
musharraf504 <124203520+musharraf504@users.noreply.github.com>
2023-05-01 01:40:20 +0530

    style add

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getMatchData() {
 
-    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=bef4b6e4-0fbb-4e11-9410-6e16e2ca5ab1&offset=0")
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=7d320629-4a6b-47a5-91a7-afa463f2ef45")
         .then(data => data.json())
         .then(data => {
             if (data.status != "success")return;
@@ -10,8 +10,8 @@ async function getMatchData() {
             if(!matchesList)return [];
             
             //add your api key from cricketdata.org
-            const relevantData = matchesList.filter(match => match.series_id == "{your_api_key}").map(match => `${match.name}, ${match.status}`);
-
+            const relevantData = matchesList.filter(match => match.series_id == "c75f8952-74d4-416f-b7b4-7da4b4e3ae6e").map(match => `${match.name}, ${match.status}.<br> <img src="${match.teamInfo[0].img}" alt=""> ${match.teamInfo[0].shortname}- ${match.score[0].r}/${match.score[0].w}, (${match.score[0].o}) <br> <img src="${match.teamInfo[1].img}" alt=""> ${match.teamInfo[1].shortname}- ${match.score[1].r}/${match.score[1].w}, (${match.score[1].o})`);
+            
             console.log({relevantData});
 
             document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
